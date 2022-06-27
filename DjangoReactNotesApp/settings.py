fa73b80f24420b08915211510c4d5b595b4dafd1
Jose Santos <67150374+jose-vs@users.noreply.github.com>
2022-06-27 16:27:41 +1200

    fixed cors and rendered notes list

--- DjangoReactNotesApp/settings.py
+++ DjangoReactNotesApp/settings.py
@@ -37,20 +37,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'api.apps.ApiConfig', 
-    
+
+    'api.apps.ApiConfig',
+
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    "corsheaders.middleware.CorsMiddleware",
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+
 ]
 
 ROOT_URLCONF = 'DjangoReactNotesApp.urls'
@@ -125,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -2,14 +2,31 @@ import React, {
     useState,
     useEffect
 } from 'react'
+import ListItem from '../components/ListItem'
 
 const NotesListPage = () => {
 
-    let [notes, setNotes] = useState([])
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => { 
+        getNotes()
+        
+    }, [])
+
+    let getNotes = async () => { 
+        var response = await fetch('http://127.0.0.1:8000/api/notes/')
+        var data = await response.json()
+        setNotes(data)
+    
+    }
 
   return (
     <div>
-        notes
+       <div className="notes-list">
+            {notes.map((note, index) => { 
+                return <ListItem key={index} note={note}/>
+            })}
+       </div>
     </div>
   )
 }
