93b168fdc4c50b029f878b15406f804aa776b4d4
Jose Santos <67150374+jose-vs@users.noreply.github.com>
2022-06-27 15:28:07 +1200

    setup notes page

---
+++ frontend/src/pages/NotesListPage.js
@@ -0,0 +1,17 @@
+import React, {
+    useState,
+    useEffect
+} from 'react'
+
+const NotesListPage = () => {
+
+    let [notes, setNotes] = useState([])
+
+  return (
+    <div>
+        notes
+    </div>
+  )
+}
+
+export default NotesListPage
