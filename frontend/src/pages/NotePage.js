94542fc70e29685410236d4e89de44742a60f3a4
Jose Santos <67150374+jose-vs@users.noreply.github.com>
2022-06-28 14:17:12 +1200

    added updating notes

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,32 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
 const NotePage = () => {
   let { id } = useParams();
   let [note, setNote] = useState(null);
+  let navigate = useNavigate();
 
   useEffect(() => {
+    const getNote = async () => {
+      const response = await fetch(`/api/notes/${id}`);
+      const data = await response.json();
+      setNote(data);
+    };
     getNote();
   }, [id]);
 
-  const getNote = async () => {
-    const response = await fetch(`/api/notes/${id}`);
-    const data = await response.json();
-    console.log(data);
-    setNote(data);
+  const updateNote = async () => {
+    fetch(`/api/notes/${id}/update/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
+  const handleSubmit = () => {
+    updateNote();
+    navigate("/", { replace: true });
   };
 
   return (
     <div className="note">
       <div className="note-header">
         <h3>
-          <Link to="/">
-            <ArrowLeft />
-          </Link>
+          <ArrowLeft onClick={handleSubmit} />
         </h3>
       </div>
-      <textarea defaultValue={note?.body}></textarea>
+      <textarea
+        onChange={(e) => {
+          setNote({ ...note, body: e.target.value });
+        }}
+        defaultValue={note?.body}
+      ></textarea>
     </div>
   );
 };
