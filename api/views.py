30086e9162f9a65b987b99313045814fed2bb709
Jose Santos <67150374+jose-vs@users.noreply.github.com>
2022-06-28 14:29:25 +1200

    added delete

--- api/views.py
+++ api/views.py
@@ -45,7 +45,7 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getNotes(request): 
-    notes = Note.objects.all()
+    notes = Note.objects.all().order_by('-updated')
     serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
 
@@ -65,3 +65,10 @@ def updateNote(request, pk):
         serializer.save()
     
     return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteNote(request, pk):
+    note = Note.objects.get(id=pk)
+    note.delete()
+    return Response('Note was deleted') 
+    
--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -16,18 +16,24 @@ const NotePage = () => {
     getNote();
   }, [id]);
 
+  const deleteNote = async () => {
+    await fetch(`/api/notes/${id}/delete/`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    navigate("/", { replace: true });
+  };
+
   const updateNote = async () => {
-    fetch(`/api/notes/${id}/update/`, {
+    await fetch(`/api/notes/${id}/update/`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(note),
     });
-  };
-
-  const handleSubmit = () => {
-    updateNote();
     navigate("/", { replace: true });
   };
 
@@ -35,8 +41,9 @@ const NotePage = () => {
     <div className="note">
       <div className="note-header">
         <h3>
-          <ArrowLeft onClick={handleSubmit} />
+          <ArrowLeft onClick={updateNote} />
         </h3>
+        <button onClick={deleteNote}>Delete</button>
       </div>
       <textarea
         onChange={(e) => {
